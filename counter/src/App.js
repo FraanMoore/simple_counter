1c86ddb4c9499f891164aaef4d477e5baa2d9898
FraanMoore <fraanmoore@gmail.com>
2023-02-04 11:31:40 -0300

    ultimo check

--- counter/src/App.js
+++ counter/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock } from '@fortawesome/free-solid-svg-icons'
 
+
 const App = (props) => {
     return (
         <div>
--- counter/src/index.js
+++ counter/src/index.js
@@ -1,8 +1,11 @@
-import App from "./App";
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let second = 0;
